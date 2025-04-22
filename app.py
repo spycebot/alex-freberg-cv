577203a524b6e7db73e77e3551c7ff2b14482324
Shannon Ware <photius@shannonware.com>
2025-04-22 08:47:36 +0100

    Push to twst Google tag

--- app.py
+++ app.py
@@ -122,8 +122,8 @@ def resume_sql():
 @app.get('/articles/flask-heroku')
 def article_flask_heroku():
     # return 'This is a test'
-    # return render_template('article-flask-heroku.html')
-    return render_template('index.html')
+    return render_template('article-flask-heroku.html')
+    # return render_template('index.html')
 
 if __name__ == '__main__':
     app.run() # debug=True, host='0.0.0.0'
